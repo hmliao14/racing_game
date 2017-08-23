0edab1242956b36e4a4c1cdde64fb1a1ec282e04
Huan Ming Liao <hmliao14@gmail.com>
2017-08-23 13:56:24 -0700

    added initial racing track logic

--- scripts/app.js
+++ scripts/app.js
@@ -5,36 +5,83 @@ $(document).ready(function(){
 
 	var p1 = $("#p1");
 	var p2 = $("#p2");
+	var t1 = $("#t1");
+	var t2 = $("#t2");
+
+	// t1.animate({
+	// 	// track starts
+	// 	"margin-top": "+=30px"
+	// }, 100);
 
 	// Keypad A.S for P1 
 	$(document).on("keydown", function(e){
+		/*
+			##### PLAYER 1 #####
+		*/
+
+
 		if(e.which === 65){
 			// Pressed 'a' to move left
-			$("#p1").animate({
+			p1.animate({
 				"margin-left": "-=30px"
 
 			}, 100);
 		} else if(e.which === 83){
-			// Pressed 'a' to move left
-			$("#p1").animate({
+			// Pressed 's' to move right
+			p1.animate({
 				"margin-left": "+=30px"
 
 			}, 100);
 		}
+		if(e.which === 69){
+			// Pressed 'e' speed up
+			t1.animate({
+				"margin-top": "+=30px"
 
+			}, 100);
+		} else if(e.which === 82){
+			// Pressed 'r' to shoot
+			// coming soon..
+			t1.animate({
+				"margin-top": "-=30px"
+
+			}, 100);
+		}
+
+		/*
+			##### PLAYER 2 #####
+		*/
 		if(e.which === 75){
-			// Pressed 'a' to move left
-			$("#p2").animate({
+			// Pressed 'k' to move left
+			p2.animate({
 				"margin-left": "-=30px"
 
 			}, 100);
 		} else if(e.which === 76){
-			// Pressed 'a' to move left
-			$("#p2").animate({
+			// Pressed 'l' to move left
+			p2.animate({
 				"margin-left": "+=30px"
 
 			}, 100);
 		}
+
+		if(e.which === 219){
+			// Pressed '[' speed up
+			t2.animate({
+				"margin-top": "+=30px"
+
+			}, 100);
+		} else if(e.which === 221){
+			// Pressed ']' to shoot
+			// coming soon..
+			t2.animate({
+				"margin-top": "-=30px"
+
+			}, 100);
+
+		}
+
+
 
 	});
 
